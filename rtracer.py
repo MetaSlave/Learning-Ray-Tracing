f3b2ff3965b074d1110e109021493fe1d165281d
MetaSlave <chongtinghao@gmail.com>
2019-07-03 15:33:13 +0800

    Done Canvas Drawing and Pretty Much Everything except for 3D dot product Calculation

--- rtracer.py
+++ rtracer.py
@@ -1,6 +1,8 @@
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
+from PIL import Image
+
 import numpy as np
+import math
+import operator
 
 class Pixel:
 	def __init__(self, x, y, z, rgb):
@@ -42,44 +44,89 @@ def Canvas_to_Viewport(x,y,width_scale,height_scale):
 	return x,y,d
 
 # Tracing Ray (Computes intersection of ray with every sphere and return color of sphere at nearest intersection.)
+def TraceRay(o,x,y,d,t_min,t_max,list_of_spheres,BACKGROUND_COLOR):
+	closest_t = float('inf')
+	closest_sphere = None
+	for sphere in list_of_spheres:
+		t1,t2 = SolveIntersect(x,y,d,o,sphere)
+		if (t_min < t1 < t_max) and (t1 < closest_t):
+			closest_t = t1
+			closest_sphere = sphere
+		if (t_min < t2 < t_max) and (t2 < closest_t):
+			closest_t = t2
+			closest_sphere = sphere
+	print(t1,t2)
+	if closest_sphere is None:
+		print("Pixel " + str(x) + "," + str(y) + " is background.")
+		return BACKGROUND_COLOR
+	print("Pixel" + str(x) + "," + str(y) + " is sphere.")
+	return closest_sphere.color
+
 # Ax * Bx + Ay * By + Az * Bz
 # Solve Quadratic Eqn
 def SolveIntersect(x,y,d,o,sphere):
 	c = sphere.center
 	r = sphere.radius
 	#oc = o - C
-	for dimension in o:
-		dimension = dimension + C[o.index(dimension)]
+	o = list(map(operator.sub,c,o))
+	#Change to 3D Calculation
+	oc = o
 	k1 = (x*x) + (y*y)
 	k2 = 2 * ((oc[0]*x) + (oc[1]*y))
 	k3 = ((oc[0]*oc[0]) + (oc[1]*oc[1])) - (r*r)
-	discriminant = k2*k2 - 4*k1*k3
+	discriminant = (k2*k2) - (4*k1*k3)
 	if discriminant < 0:
 		return false,false
-	t1 = ((-k2) + sqrt(discriminant)) / (2*k1)
-	t2 = ((-k2) - sqrt(discriminant)) / (2*k1)
+	if k1 == 0:
+		t1 = 0
+		t2 = 0
+	else:
+		t1 = ((-k2) + math.sqrt(discriminant)) / (2*k1)
+		t2 = ((-k2) - math.sqrt(discriminant)) / (2*k1)
 	return t1,t2
 
 # Create Spheres in Main
 def Main():
-	# Initialize Pixels in Canvas
-	canvas_width = 100
-	canvas_height = 100
-	canvas_empty_pixel = [0,0,0]
-	canvas_pixel_list = []
-	for x in range(0,len(canvas_width)):
-		for y in range(0,len(canvas_height)):
-			canvas_pixel_list.append(Pixel(x,y,d,canvas_empty_pixel))
 
 	# Need to Dot Product o
 	o = [0,0,0]
+	BACKGROUND_COLOR = [0,0,0]
+	t_min = 1
+	t_max = float('inf')
+	list_of_colors = []
+
 	# List of Spheres
 	list_of_spheres = []
 	s1 = Sphere([0,-1,3],1,[255,0,0])
 	list_of_spheres.append(s1)
-	for sphere in list_of_spheres:
-		t1,t2 = SolveIntersect(x,y,d,o,sphere)
 
+	# Initialize Pixels in Canvas
+	canvas_width = 100
+	canvas_height = 100
+	width_scale = 1
+	height_scale = 1
+	canvas_empty_pixel = [0,0,0]
+	canvas_pixel_list = []
+	for x in range(0,canvas_width):
+		for y in range(0,canvas_height):
+			canvas_pixel_list.append(Pixel(x,y,False,canvas_empty_pixel))
+
+	# Trace Pixels in Canvas
+	for pixel in canvas_pixel_list:
+		x,y,d = Canvas_to_Viewport(pixel.x,pixel.y,width_scale,height_scale)
+		color = TraceRay(o,x,y,d,t_min,t_max,list_of_spheres,BACKGROUND_COLOR)
+		pixel.rgb = color
+		list_of_colors.extend(color)
+
+	# Plot to Image
+	list_of_colors = bytes(list_of_colors)
+	img = Image.frombytes('RGB',(canvas_width,canvas_height),list_of_colors)
+	img.show()
+	img.save('test.png')
+
+	# Coordinate Calculator len(list)%width = y len(list)-len(list)%width
+if __name__ == "__main__":
+	Main()
 '''
 # Empty 2D Canvas Generator
 canvas_width = 100
